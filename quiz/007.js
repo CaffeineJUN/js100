04fad24a75b5b2f8c118cfab03d8ac7e710cb876
강혁준 <gurwns232@naver.com>
2022-11-22 23:38:09 +0900

    007

---
+++ quiz/007.js
@@ -0,0 +1,11 @@
+let age = 20;
+let Age = 20;
+// let let = 20
+let _age = 20;
+// let 1age = 20
+
+console.log(age);
+console.log(Age);
+// console.log(let);
+console.log(_age);
+// console.log(1age);

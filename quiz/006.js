8aea81b8ffb0f6b83adafcfd1422892a967fed0d
강혁준 <gurwns232@naver.com>
2022-11-22 22:53:56 +0900

    006

---
+++ quiz/006.js
@@ -0,0 +1,14 @@
+const a = Boolean(NaN);
+const b = Boolean(1);
+const c = Boolean('');
+const d = Boolean(0);
+const e = Boolean(undefined);
+//
+const f = new Boolean(false);
+
+console.log(a.toString());
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
